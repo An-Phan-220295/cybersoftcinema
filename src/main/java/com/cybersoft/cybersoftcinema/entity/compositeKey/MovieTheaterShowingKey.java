e8decbc8029fe077efdc90be0730f4e33ede0fd1
An Phan <phucanphan1995@gmail.com>
2023-10-30 11:29:37 +0700

    updated EntityKey

---
+++ src/main/java/com/cybersoft/cybersoftcinema/entity/compositeKey/MovieTheaterShowingKey.java
@@ -0,0 +1,41 @@
+package com.cybersoft.cybersoftcinema.entity.compositeKey;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class MovieTheaterShowingKey implements Serializable {
+    @Column(name = "idMovie", nullable = false)
+    private int idMovie;
+
+    @Column(name = "idTheater", nullable = false)
+    private int idTheater;
+
+    @Column(name = "idShowing", nullable = false)
+    private int idShowing;
+
+    public int getIdMovie() {
+        return idMovie;
+    }
+
+    public void setIdMovie(int idMovie) {
+        this.idMovie = idMovie;
+    }
+
+    public int getIdTheater() {
+        return idTheater;
+    }
+
+    public void setIdTheater(int idTheater) {
+        this.idTheater = idTheater;
+    }
+
+    public int getIdShowing() {
+        return idShowing;
+    }
+
+    public void setIdShowing(int idShowing) {
+        this.idShowing = idShowing;
+    }
+}
